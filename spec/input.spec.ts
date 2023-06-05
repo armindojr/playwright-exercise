61842bec7afb0f4ec2bbc73019969a651798accc
Armindo Jr <armindo.junior05@gmail.com>
2023-06-05 19:14:50 -0300

    Feature: Updating pw and tests

---
+++ spec/input.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Interacting with inputs', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/edit');
+    });
+
+    test('Filling an input', async ({ page }) => {
+        await page.fill('input#fullName', 'Armindo Junior');
+        let text = await page.locator('input#fullName').inputValue();
+        expect(text).toEqual('Armindo Junior');
+    });
+
+    test('Pressing key', async ({ page }) => {
+        await page.fill('input#join', 'Foo Bar');
+        await page.keyboard.press('Tab');
+    });
+
+    test('Checking text', async ({ page }) => {
+        let text = await page.locator('input#getMe').inputValue();
+        expect(text).toEqual('ortonikc');
+    });
+
+    test('Clearing input', async ({ page }) => {
+        await page.locator('input#clearMe').clear();
+        let text = await page.locator('input#clearMe').inputValue();
+        expect(text).toEqual('');
+    });
+
+    test('Checking if input is disabled', async ({ page }) => {
+        let status = await page.locator('input#noEdit').isDisabled();
+        expect(status).toEqual(true);
+    });
+
+    test('Checking if input is read only', async ({ page }) => {
+        let status = await page.locator('input#dontwrite').isEditable();
+        expect(status).toEqual(false);
+    });
+});
